d09c9b9e6f5d457275fc057b662992ef47807f21
unknown <kdkrage@gmail.com>
2022-05-06 10:42:29 -0400

    prointro added

---
+++ src/prointro.cpp
@@ -0,0 +1,56 @@
+#include <windows.h>
+#include <stdio.h>
+#include "detours.h"
+#include "detver.h"
+#include "syelog.h"
+#pragma comment(lib, "detours.lib")
+#pragma comment(lib, "user32.lib")
+
+
+// Address of the real CloseHandle API
+BOOL (WINAPI *True_CloseHandle)(HANDLE) = CloseHandle;
+ 
+// Our intercept function
+BOOL WINAPI HookedCloseHandle(HANDLE hObject)
+{
+    printf("This works");
+    return True_CloseHandle(hObject);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID p)
+{
+    switch (dwReason) {
+        case DLL_PROCESS_ATTACH:
+        {
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourAttach(&(PVOID&)True_CloseHandle, HookedCloseHandle);
+    
+            LONG lError = DetourTransactionCommit();
+            if (lError != NO_ERROR) {
+                MessageBox(NULL, "Process Introspection", "DLL NOT INJECTED", MB_ICONINFORMATION|MB_OK);
+                return FALSE;
+            }
+        }
+        break;
+    
+        case DLL_PROCESS_DETACH:
+        {
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourDetach(&(PVOID&)True_CloseHandle, HookedCloseHandle);
+    
+            LONG lError = DetourTransactionCommit();
+            if (lError != NO_ERROR) {
+                MessageBox(NULL, "Process Introspection", "DLL NOT INJECTED", MB_ICONINFORMATION|MB_OK);
+                return FALSE;
+            }
+        }
+        break;
+    }
+}
+
+
+
+
+
